d351c8fedc747991c6b1114a8382b7ceaa4756ac
Daniel Arzanipour <daniel.arzanipour@gmail.com>
2024-09-03 11:20:33 -0400

    add templates and have a flash card be created using them on form submission

--- script.js
+++ script.js
@@ -176,6 +176,14 @@ function handleCreateNewCardFormSubmission(form) {
         closeModal('modal-new_card', 'form-new_card');
 
         saveNewCard();
+
+        // append the new card
+        const playArea = document.querySelector('.play-area');
+
+        if (playArea instanceof HTMLDivElement) {
+          appendTemplate(playArea, frontCardTemplate, 'First Question');
+          appendTemplate(playArea, backCardTemplate, 'First Answer');
+        }
       },
       { once: true }
     );
@@ -261,23 +269,87 @@ function loadAllCards() {
 
 //^ TEMPLATES
 /**
- *
- * @param {*} title
- * @param {*} body
+ * This is a template for the front of the flash card
+ * @param {string} question - The question being asked
+ * @returns {HTMLElement | null}
  */
-function createCardElement(title, body) {
-  // const template = document.getElementById('card_template');
-  // const element = template.content.cloneNode(true).firstElementChild;
-  // const [cardTitle] = element.getElementsByTagName("h3");
-  // const [cardBody] = element.getElementsByTagName("section");
-  // [cardTitle.textContent, cardBody.textContent] = [title, body];
-  // return element;
+function frontCardTemplate(question) {
+  const template = document.getElementById('card-front-template');
+
+  if (template instanceof HTMLTemplateElement) {
+    const node = template.content.cloneNode(true);
+    // @ts-ignore
+    const element = node.firstElementChild;
+
+    if (element instanceof HTMLElement) {
+      const [cardQuestion] = element.getElementsByTagName('p');
+      if (cardQuestion) {
+        cardQuestion.textContent = question;
+        return element;
+      } else {
+        console.error('No <p> element found in the template.');
+        return null;
+      }
+    } else {
+      console.error('First element child is not an HTMLElement.');
+      return null;
+    }
+  } else {
+    console.error(
+      'Template with ID card-front-template not found or is not an HTMLTemplateElement.'
+    );
+    return null;
+  }
 }
-// 👆
-// container.appendChild(createCardElement(
-//     "Frontend System Design: Fundamentals",
-//     "This is a random content"
-//   ))
+
+/**
+ * This is a template for the back of the flash card
+ * @param {string} answer - The answer to the question on the front of the flash card
+ */
+function backCardTemplate(answer) {
+  const template = document.getElementById('card-back-template');
+
+  if (template instanceof HTMLTemplateElement) {
+    const node = template.content.cloneNode(true);
+    // @ts-ignore
+    const element = node.firstElementChild;
+
+    if (element instanceof HTMLElement) {
+      const [cardQuestion] = element.getElementsByTagName('p');
+      if (cardQuestion) {
+        cardQuestion.textContent = answer;
+        return element;
+      } else {
+        console.error('No <p> element found in the template.');
+        return null;
+      }
+    } else {
+      console.error('First element child is not an HTMLElement.');
+      return null;
+    }
+  } else {
+    console.error(
+      'Template with ID card-front-template not found or is not an HTMLTemplateElement.'
+    );
+    return null;
+  }
+}
+
+/**
+ * This will take a flash card template and append it to a specified container
+ * @param {HTMLElement} container - The container to append the template to
+ * @param {function} template - The template creation function
+ * @param {string} text - The text content of the element
+ */
+function appendTemplate(container, template, text) {
+  const cardElement = template(text);
+
+  if (cardElement) {
+    container.appendChild(cardElement);
+  } else {
+    console.error('Failed to create the element.');
+  }
+}
 
 //^ BATCH INSERTS
 // const fragment = document.createDocumentFragment();
